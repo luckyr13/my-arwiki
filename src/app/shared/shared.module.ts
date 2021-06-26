78b4c1653090735d05e3a44d99a9e516cc6a5f11
technodromex <ricardo.guzmantorres@owasp.org>
2021-06-25 20:17:48 -0500

    Add ripples :)

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import {MatRippleModule} from '@angular/material/core';
 
 
 
@@ -11,6 +12,7 @@ import {MatButtonModule} from '@angular/material/button';
   ],
   exports: [
     MatButtonModule,
+    MatRippleModule
   ]
 })
 export class SharedModule { }
